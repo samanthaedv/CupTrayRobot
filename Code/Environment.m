a4e0e5611cf60413a668d1937010edb785859310
samanthaedv <samantha.e.edvell@student.uts.edu.au>
2024-10-28 19:44:41 +1100

    Environment changes

--- Code/Environment.m
+++ Code/Environment.m
@@ -4,23 +4,25 @@ clc;
 clf;
 clear all;
 hold on;
-% axis([-2 2 -2 2 -1 3])
-% 
-% Bench([1,0,0,0;
-%       0,1,0,0;
-%       0,0,1,0;
-%       0,0,0,1]);
+axis([-2 2 -2 2 -1 3])
+
+Bench([1,0,0,0;
+      0,1,0,0;
+      0,0,1,0;
+      0,0,0,1]);
 
 firstAidModel = PlaceObject('FirstAidKit.ply')
-% PlaceObject('estop.ply')
-% 
-% 
 vertices = [get(firstAidModel,'Vertices'), ones(size(get(firstAidModel,'Vertices'),1),1)];
-            transformedVertices = vertices;
-% 
-   set(firstAidModel,'Vertices',transformedVertices(:,1:3));
+            transformedVertices = vertices*transl(-0.7,12,8.4)'*0.25;
+set(firstAidModel,'Vertices',transformedVertices(:,1:3));
 
-            drawnow;
+
+eStopModel = PlaceObject('estop.ply')
+vertices = [get(eStopModel,'Vertices'), ones(size(get(eStopModel,'Vertices'),1),1)];
+            transformedVertices = vertices*transl(0,0,1)';
+set(eStopModel,'Vertices',transformedVertices(:,1:3));
+
+drawnow;
 
 
 
