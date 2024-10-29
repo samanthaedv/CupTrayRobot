19a1ce4f4a796bd47e5fc41bc4552a75270b29f8
samanthaedv <samantha.e.edvell@student.uts.edu.au>
2024-10-29 17:06:01 +1100

    Environment Update

--- Code/Environment.m
+++ Code/Environment.m
@@ -1,8 +1,5 @@
 function Environment
 
-clc;
-clf;
-clear all;
 hold on;
 axis([-2 2 -2 2 -1 3])
 
@@ -19,11 +16,17 @@ set(firstAidModel,'Vertices',transformedVertices(:,1:3));
 
 eStopModel = PlaceObject('estop.ply')
 vertices = [get(eStopModel,'Vertices'), ones(size(get(eStopModel,'Vertices'),1),1)];
-            transformedVertices = vertices*transl(0,0,1)';
+            transformedVertices = vertices*(transl(1.4,-0.4,0.9)*trotx(pi/2))';
 set(eStopModel,'Vertices',transformedVertices(:,1:3));
 
-drawnow;
 
+spillKitModel = PlaceObject('SpillKit.ply')
+vertices = [get(spillKitModel,'Vertices'), ones(size(get(spillKitModel,'Vertices'),1),1)];
+            transformedVertices = vertices*(transl(2.3,-0.2,0)*trotx(pi/2)*troty(pi))'*0.75;
+set(spillKitModel,'Vertices',transformedVertices(:,1:3));
+
+
+drawnow;
 
 
 
